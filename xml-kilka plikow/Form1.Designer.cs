31168cbf2053666e32d4d101ffe09f17676c8d46
lesny23wp <lesniewski90@gmail.com>
2016-12-13 23:40:18 +0100

    Add files via upload

---
+++ xml-kilka plikow/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace xml_kilka_plikow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Podaj lokalizacje folderu";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(157, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(278, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "E:\\XMLFiles";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Lime;
+            this.button2.Location = new System.Drawing.Point(33, 158);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 64);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Połącz pliki";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(157, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(278, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "E:\\wynik.xml";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(33, 69);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 48);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Podaj lokalizalcję pliku wynikowego";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(153, 225);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 5;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(157, 126);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(384, 96);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 254);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Łączenie plików XML";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ xml-kilka plikow/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+using System.Linq.Expressions;
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
+
+
+namespace xml_kilka_plikow
+{
+    public partial class Form1 : Form
+    {
+        string sciezka1;
+        string sciezka2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+ 
+            FolderBrowserDialog folderbrowser1 = new FolderBrowserDialog();
+
+            folderbrowser1.ShowDialog();
+
+            textBox1.Text = folderbrowser1.SelectedPath.ToString();
+
+            sciezka1 = textBox1.Text;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sciezka1 = textBox1.Text;
+                var downloadfolder = sciezka1;
+                string[] files = Directory.GetFiles(downloadfolder);
+                var masterfile = new XDocument();
+
+                XElement dokument = new XElement("ProfitsCosts");
+
+
+                masterfile.Add(dokument);
+
+                foreach (var file in files)
+                {
+
+                    XDocument xdoc = XDocument.Load(file);
+
+                    masterfile.Root.Add(xdoc.Descendants("ProfitsCostsEntry"));
+
+                }
+
+
+                //zapis
+                sciezka2 = textBox2.Text;
+
+                masterfile.Save(sciezka2);
+
+
+                int dlugoscTablicy;
+                dlugoscTablicy = files.Length;
+
+                string dd = files[0];
+                string ddostatni = files[dlugoscTablicy - 1];
+
+
+                string calosc1 = System.IO.File.ReadAllText(dd);
+
+                Match datap = Regex.Match(calosc1, "<From.*From>");
+
+                string calosc2 = System.IO.File.ReadAllText(ddostatni);
+
+                Match datak = Regex.Match(calosc2, "<To.*To>");
+
+
+                string wynik_bez_dat = System.IO.File.ReadAllText(sciezka2);
+                string zastap = "<AccountingProfitsCostsReport>";
+
+                string wynik_z_dat = Regex.Replace(wynik_bez_dat, "<ProfitsCosts>", zastap + Environment.NewLine + "\t" + datap + Environment.NewLine + "\t" + datak + Environment.NewLine + "<ProfitsCosts>");
+
+                string koncowka = "\r\n</AccountingProfitsCostsReport>";
+                string wyn = wynik_z_dat + koncowka;
+
+                string plik = (sciezka2);
+                
+                //string plik = sciezka3;
+                StreamWriter sw = new StreamWriter(plik, false);
+                sw.Write(wyn);
+                sw.Close();
+
+                label1.Text = "Zakończono łączenie!";
+
+                int n = 0;
+
+
+                while (n != dlugoscTablicy)
+                {
+
+
+                    richTextBox1.AppendText(files[n] + "\r\n");
+
+                    n++;
+                }
+
+
+            }
+            catch (ArgumentNullException)
+            {
+                MessageBox.Show("Nie wybrano pliku!");
+
+            }
+
+            catch (Exception)
+            {
+                if (textBox2.Text == "")
+                {
+                    MessageBox.Show("Nie podano sciezki!");
+                }
+                else
+                {
+                    MessageBox.Show("Błędna ściezka lub wybrano folder bez plików xml!");
+                }
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "xml files (*.xml)|*.xml|All files (*.*)|*.*";
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            textBox2.Text = saveFileDialog1.FileName;
+        }
+    }
+}
