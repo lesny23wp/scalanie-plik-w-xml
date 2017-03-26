a3dd0d889538d7590fb1d0f097b3b22f0580a813
lesny23wp <lesniewski90@gmail.com>
2017-03-26 23:47:46 +0200

    test commit
    Add test

--- xml-kilka plikow/Program.cs
+++ xml-kilka plikow/Program.cs
@@ -8,15 +8,18 @@ namespace xml_kilka_plikow
 {
     static class Program
     {
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+	    //Test
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
